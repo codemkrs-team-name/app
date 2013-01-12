5dd76efca08cb9a37e2bf070fdcc7af9ee3e1096
gmauer <gmauer@gmauer-ThinkPad-T420.(none)>
2013-01-12 10:30:17 -0600

    bf.js refactoring

--- prototypes/scrapers/barryfest/bf.js
+++ prototypes/scrapers/barryfest/bf.js
@@ -3,10 +3,11 @@ var  sys 			= require('util')
 		,xml2js		= require('xml2js')
 		,_ 				= require('underscore')
 		
+    ,source   = 'http://www.google.com/calendar/feeds/matthew.r.rosenthal%40gmail.com/public/basic'
 		,time 		= /When:\s*([^<]*)<.*/
 		,location = /Where:\s*([^<]*)<.*/
 		;
-rest.get('http://www.google.com/calendar/feeds/matthew.r.rosenthal%40gmail.com/public/basic').on('complete', function(result) {
+rest.get(source).on('complete', function(result) {
 	var  parser = new xml2js.Parser()
 			;
   if (result instanceof Error) 
@@ -15,7 +16,7 @@ rest.get('http://www.google.com/calendar/feeds/matthew.r.rosenthal%40gmail.com/p
   parser.parseString(result, function (err, result) {
   	var entries = _.first(_.map(result.feed.entry, function(x){
   		return {
-  			 event: x.title[0]._
+  			 eventName: x.title[0]._
   			,location: fix(x.content[0]._.match(location)[1])
   			,time: fix(x.content[0]._.match(time)[1])
   			,image: "?"
