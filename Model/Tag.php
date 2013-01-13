c990bd159e4dda8a1b74083c68680e8d39a3bed4
Joel Carranza <joel.carranza@gmail.com>
2013-01-12 21:47:15 -0600

    delete all this shit

--- Model/Tag.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Tag extends AppModel {
-
-	public $name = 'Tag';
-	public $order = array('count' => 'DESC');
-	public $hasAndBelongsToMany = array('Project');	
-	
-}//end class
-?>
--- View/Events/index.ctp
+++
@@ -1,6 +0,0 @@
-<?php
-
-phpinfo();
-
-debug($results);
-?>
