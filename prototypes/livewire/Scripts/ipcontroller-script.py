12fc4703bbfb4406472848e117066e534b3c1350
Jason Francis <halfspiral@gmail.com>
2013-01-12 19:54:21 -0600

    Dates & duplicate removal

---
+++ prototypes/livewire/Scripts/ipcontroller-script.py
@@ -0,0 +1,9 @@
+#!C:\Users\jgardino\Documents\GitHub\app\prototypes\livewire\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'ipython==0.13.1','console_scripts','ipcontroller'
+__requires__ = 'ipython==0.13.1'
+import sys
+from pkg_resources import load_entry_point
+
+sys.exit(
+   load_entry_point('ipython==0.13.1', 'console_scripts', 'ipcontroller')()
+)
